8358841dd4773b2d71cd114dcb5b9f2438aef2d4
Wilhelm Berghammer <58391294+wilhelmberghammer@users.noreply.github.com>
2020-01-12 10:34:52 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF443E64),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
